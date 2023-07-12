a26014a62ef33b3b3db140eca532c908ca281dff
DhruvKumar Nagar <iamdhruvnagar@gmail.com>
2023-07-13 04:27:59 +0530

    Added code for API to get analytics of url

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const { ServerConfig } = require("./config");
-
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
 const app = express();
+const URL = require("./models/url");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+
+connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(
+  console.log("MongoDB connected!")
+);
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
 
 app.listen(ServerConfig.PORT, () => {
   console.log("We are able to connect with PORT,", ServerConfig.PORT);
